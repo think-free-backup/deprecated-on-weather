c4d44c8a732df107ce3dba4a5b25cd8cec88c192
Meurice Christophe <meumeu1402@gmail.com>
2013-01-24 19:42:50 +0000

    mkdir databases

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir databases
+npm install sqlite3 restify
+
+clear
+echo ""
+echo "Ensure that you have the backend you want compiled in the folder backend"
+echo "All compilations provided are for the raspberry pi (for the moment)"
+echo ""
+
+echo "Change the config file as you want"
+echo ""
